058fd246beab0e22b9253e28f093ecd143a15d7e
Sébastien Delafond <sdelafond@gmail.com>
2019-11-18 15:04:53 +0100

    Bring our sid Dockerfile closer to recent upstream version based on buster

--- debian/tests/docker/testscript.sh
+++ debian/tests/docker/testscript.sh
@@ -6,13 +6,7 @@ source functions.sh
 [ -d /srv/cassandane.git ] || _cassandaneclone
 #_cyrusbuild
 apt-get update
-# FIXME: move libdbi-perl into Dockerfile.jessie
-DEBIAN_FRONTEND=noninteractive apt-get install -y libdbi-perl
 
-# FIXME: move libhttp-daemon-perl into Dockerfile.sid
-DEBIAN_FRONTEND=noninteractive apt-get install -y libhttp-daemon-perl
-
-# FIXME: install package from autopkgtest
 DEBIAN_FRONTEND=noninteractive apt-get install -y cyrus-imapd
 
 _updatejmaptestsuite
